bf4c9f636d11a5c5705bf85bf3e6972fcc7d852f
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-02-12 14:12:56 +0530

    Update insertion-operator-endl.cpp

---
+++ insertion-operator-endl.cpp
@@ -0,0 +1,9 @@
+// Insertion operator is also used as Bitwise left-shift operator (<<)
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Name : Sarthak" << endl;
+    cout << "Language : C++" << endl;
+    cout << "Compiler : g++" << endl;
+    return 0;
+}
