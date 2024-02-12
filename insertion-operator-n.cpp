a689c760e0ef922056bc9cca489cbc777e9b0bf1
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-02-12 14:11:02 +0530

    Add files via upload

---
+++ insertion-operator-n.cpp
@@ -0,0 +1,9 @@
+// Insertion operator is also used as Bitwise left-shift operator (<<)
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Name : Sarthak\n";
+    cout << "Language : C++\n";
+    cout << "Compiler : g++\n";
+    return 0;
+}
