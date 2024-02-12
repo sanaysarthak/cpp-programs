254c7c0fb826d22b6c74c8ec84e3ee0b6fdc2117
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-02-12 14:11:52 +0530

    Add files via upload

---
+++ extraction-operator.cpp
@@ -0,0 +1,18 @@
+// Insertion operator is also used as Bitwise right-shift operator (>>)
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter First Number : ";
+    cin >> num1;
+
+    cout << "Enter Second Number : ";
+    cin >> num2;
+
+    cout << "Addition : ";
+    cout << num1 + num2;
+
+    return 0;
+}
