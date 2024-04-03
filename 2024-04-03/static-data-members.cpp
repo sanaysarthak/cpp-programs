d04cc56cb5229a2394a0f3873f6be0e1873130a2
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-04-03 10:41:15 +0530

    Create static-data-members.cpp

---
+++ 2024-04-03/static-data-members.cpp
@@ -0,0 +1,28 @@
+// static keyword is used as prefix
+// it is initialized with 0
+// its scoping is for the entire program, just like a global variable
+
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+    private:
+        static int count;
+    public:
+        void getCount()
+        {
+            cout << "Count = " << ++count << endl;
+        }
+};
+
+int Demo :: count;
+
+int main()
+{
+    Demo d1, d2, d3;
+    d1.getCount();
+    d2.getCount();
+    d3.getCount();
+    return 0;
+}
