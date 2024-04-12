8ec20b6eaa59b74442a01e94a1c7e9ede11f44ac
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-04-12 11:20:35 +0530

    Create array-of-objects.cpp

---
+++ 2024-04-12/array-of-objects.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+	private:
+		double length;
+		double width;
+
+	public:
+		Rectangle(double l = 0, double w = 0) {} // Default constructor
+		void setDimensions(double l, double w) {
+			length = l;
+			width = w;
+		}
+		double area() const {
+			return length * width;
+		}
+};
+
+int main() {
+	const int size = 3; // Number of objects to be created
+	Rectangle rectangles[size]; // Array of Rectangles objects
+	// Set dimensions for each rectangle
+	rectangles[0].setDimensions(5.0, 3.0);
+	rectangles[1].setDimensions(4.0, 4.0);
+	rectangles[2].setDimensions(6.0, 2.5);
+	// Display area of each rectangle
+	for(int i=0; i<size; i++) {
+		cout << "Rectangle " << i+1 << " Area: " << rectangles[i].area() << endl;
+	}
+	return 0;
+}
