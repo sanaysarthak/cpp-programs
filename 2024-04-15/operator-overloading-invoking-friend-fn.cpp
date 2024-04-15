918b06724d34c45aad7f8df6e25e59fea65b2c66
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-04-15 23:26:09 +0530

    Create operator-overloading-invoking-friend-fn.cpp

---
+++ 2024-04-15/operator-overloading-invoking-friend-fn.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+	private:
+		int real, imag;
+
+	public:
+		Complex(int r=0, int i=0) {
+			real = r;
+			imag = i;
+		}
+		void display() {
+			cout << "\n" << real << "+i" << imag;
+		}
+		friend Complex operator +(Complex c1, Complex c2);
+};
+
+Complex operator +(Complex c1, Complex c2) {
+	Complex temp;
+	temp.real = c1.real + c2.real;
+	temp.imag = c1.imag + c2.imag;
+	return temp;
+}
+
+int main() {
+	Complex c1(5,3);
+	Complex c2(10,5);
+	Complex c3;
+	c1.display();
+	cout << " + ";
+	c2.display();
+	cout << " = ";
+	c3 = c1 + c2;
+	c3.display();
+	return 0;
+}
