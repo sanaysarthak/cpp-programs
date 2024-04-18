9b11f6ddfc404e17d9a98c4dc48a61267b0ae4ce
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-04-18 10:45:06 +0530

    Create overloading-prefix-postfix-operators.cpp

---
+++ 2024-04-18/overloading-prefix-postfix-operators.cpp
@@ -0,0 +1,32 @@
+// Overloading Prefix and Postfix operators
+// Using the unary operator ++
+
+#include <iostream>
+using namespace std;
+
+class Number {
+	private:
+		int m;
+
+	public:
+		Number() {
+			m = 0;
+		}
+		Number(int x) {
+			m = x;
+		}
+		void operator++() {
+			++m;
+			cout << "Pre-Increment = " << m << endl;
+		}
+		void operator++(int) {
+			m++;
+			cout << "Post-Increment = " << m << endl;
+		}
+};
+int main() {
+	Number n1(5);
+	++n1;
+	n1++;
+	return 0;
+}
