212296d8232df76c6f62591146e4c0464bd52581
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-05-16 19:49:54 +0530

    Update derived-class-constructor.cpp
    added a bit of explanation, in the form of comments

--- 2024-05-16/derived-class-constructor.cpp
+++ 2024-05-16/derived-class-constructor.cpp
@@ -1,5 +1,7 @@
 // Derived class constructor
+// An example code to showcase what will happen when your parent (base) class will have a constructor, and tben the child (derived) class object is created.
 // When an object of the derived class is created, it first calls the constructor of the base class, and then the constructor of its own class at the end.
+// It is done to initialize the data members from the parent (base) class first, in case they are needed in the child (derived) class, as we are inheriting them
 
 #include <iostream>
 using namespace std;
